1a86b6f59c65448aaa306fc0e05417bf06a4f8cc
benitmatumona <polviedivine@gmail.com>
2026-06-13 18:18:52 +0200

    updated files

---
+++ src/main/java/oopmastery/Question03_Cat.java
@@ -0,0 +1,15 @@
+package oopmastery;
+
+public class Question03_Cat extends Question03_Animal {
+
+    public Question03_Cat(String name) {
+        super(name);
+    }
+
+    @Override
+    public String makeSound() {
+        // TODO:
+        // Return "Meow"
+        return "";
+    }
+}
