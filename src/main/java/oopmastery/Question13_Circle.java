13fdc50503e863a4bf2511bf0197dd0c715bffc0
benitmatumona <polviedivine@gmail.com>
2026-06-14 23:53:37 +0200

    updated files

---
+++ src/main/java/oopmastery/Question13_Circle.java
@@ -0,0 +1,22 @@
+package oopmastery;
+
+public class Question13_Circle
+        extends Question13_Shape {
+
+    private double radius;
+
+    public Question13_Circle(
+            double radius
+    ) {
+        // TODO:
+        // Store the radius
+    }
+
+    @Override
+    public double area() {
+        // TODO:
+        // Return:
+        // Math.PI * radius * radius
+        return 0;
+    }
+}
