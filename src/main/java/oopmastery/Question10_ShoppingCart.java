e0f27524ddfef2fa6f785e7b501d2eca9219d0a8
benitmatumona <polviedivine@gmail.com>
2026-06-14 23:30:19 +0200

    updated files

---
+++ src/main/java/oopmastery/Question10_ShoppingCart.java
@@ -0,0 +1,27 @@
+package oopmastery;
+
+import java.util.List;
+
+public class Question10_ShoppingCart {
+
+    private List<Question10_Product> products;
+
+    public Question10_ShoppingCart() {
+        // TODO:
+        // Create the product list
+    }
+
+    public void addProduct(
+            Question10_Product product
+    ) {
+        // TODO:
+        // Add the product to the cart
+    }
+
+    public double totalPrice() {
+        // TODO:
+        // Calculate and return
+        // the total value of all products
+        return 0;
+    }
+}
