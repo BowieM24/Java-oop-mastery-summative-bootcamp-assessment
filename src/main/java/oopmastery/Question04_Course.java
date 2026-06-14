37e82da6628d0e4e6786d575b471f70828642c48
benitmatumona <polviedivine@gmail.com>
2026-06-14 23:12:36 +0200

    updated files

---
+++ src/main/java/oopmastery/Question04_Course.java
@@ -0,0 +1,30 @@
+package oopmastery;
+
+import java.util.List;
+
+public class Question04_Course {
+
+    private String courseName;
+    private List<Question04_Student> students;
+
+    public Question04_Course(String courseName) {
+        // TODO:
+        // Store the course name
+        // Create the student list
+    }
+
+    public void enroll(Question04_Student student) {
+        // TODO:
+        // Add the student to the course
+    }
+
+    public int studentCount() {
+        // TODO:
+        return 0;
+    }
+
+    public String getCourseName() {
+        // TODO:
+        return "";
+    }
+}
