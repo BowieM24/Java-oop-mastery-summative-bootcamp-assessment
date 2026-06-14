ea764a4590b7eadfdfbab05308a3fc911059ec6d
benitmatumona <polviedivine@gmail.com>
2026-06-14 23:10:58 +0200

    updated files

---
+++ src/main/java/oopmastery/Question04_Student.java
@@ -0,0 +1,16 @@
+package oopmastery;
+
+public class Question04_Student {
+
+    private String name;
+
+    public Question04_Student(String name) {
+        // TODO:
+        // Store the student's name
+    }
+
+    public String getName() {
+        // TODO:
+        return "";
+    }
+}
