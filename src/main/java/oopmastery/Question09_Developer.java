7b0b29570c755e9828649fb16bcf28897b686a60
benitmatumona <polviedivine@gmail.com>
2026-06-14 23:26:46 +0200

    updated files

---
+++ src/main/java/oopmastery/Question09_Developer.java
@@ -0,0 +1,16 @@
+package oopmastery;
+
+public class Question09_Developer
+        extends Question09_Employee {
+
+    public Question09_Developer(String name) {
+        super(name);
+    }
+
+    @Override
+    public double calculateBonus() {
+        // TODO:
+        // Return 2000
+        return 0;
+    }
+}
