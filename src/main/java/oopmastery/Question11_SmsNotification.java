d5d0e5e39ed846c4825f863bb628a8bb2a921a6d
benitmatumona <polviedivine@gmail.com>
2026-06-14 23:33:35 +0200

    updated files

---
+++ src/main/java/oopmastery/Question11_SmsNotification.java
@@ -0,0 +1,13 @@
+package oopmastery;
+
+public class Question11_SmsNotification
+        implements Question11_NotificationService {
+
+    @Override
+    public String send(String message) {
+        // TODO:
+        // Return:
+        // "SMS sent: " + message
+        return "";
+    }
+}
