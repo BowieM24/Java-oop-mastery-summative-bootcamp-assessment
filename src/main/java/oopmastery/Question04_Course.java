40c3c52a277f171b8125acea9d29cf545104335a
Bowie <mamonjhb025@student.wethinkcode.co.za>
2026-06-24 18:38:04 +0200

    Implement course name storage and student list management in Question04_Course class

--- src/main/java/oopmastery/Question04_Course.java
+++ src/main/java/oopmastery/Question04_Course.java
@@ -1,6 +1,8 @@
 package oopmastery;
 
 import java.util.List;
+import java.util.ArrayList;
+
 
 public class Question04_Course {
 
@@ -8,23 +10,19 @@ public class Question04_Course {
     private List<Question04_Student> students;
 
     public Question04_Course(String courseName) {
-        // TODO:
-        // Store the course name
-        // Create the student list
+        this.courseName = courseName;
+        this.students = new ArrayList<>();
     }
 
     public void enroll(Question04_Student student) {
-        // TODO:
-        // Add the student to the course
+        students.add(student);
     }
 
     public int studentCount() {
-        // TODO:
-        return 0;
+        return students.size();
     }
 
     public String getCourseName() {
-        // TODO:
-        return "";
+        return this.courseName;
     }
 }
